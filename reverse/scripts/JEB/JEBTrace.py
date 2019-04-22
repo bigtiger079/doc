d7b8b0529576ce39cef4b9d49ac4537ef9c0a190
bigtiger079 <bigtiger079@gmail.com>
2019-04-22 22:49:12 +0800

    add task script

--- reverse/scripts/JEB/JEBTrace.py
+++ reverse/scripts/JEB/JEBTrace.py
@@ -7,6 +7,9 @@ from java.lang import Runnable
 
 from com.pnfsoftware.jeb.core.actions import ActionXrefsData
 import re
+import sys
+
+sys.setrecursionlimit(100000)
 
 
 class JEBTrace(IScript):
@@ -37,7 +40,13 @@ class JEBTrace(IScript):
                         # print(m.getSignature(True), m.getAddress(), m.getAddress())
                         refs = DataReference(DataReferenceNode(m.getItemId(), m.getAddress()))
                         self.findMethodXRef(units, m.getItemId(), m.getAddress(), refs)
+                        print(refs.getNodes())
                         print(refs.getLeafNodes())
+                        lines = []
+                        for leaf in refs.getLeafNodes():
+                            lines.extend(refs.getLineToRoot(XrefLine(leaf)))
+
+                        print(lines)
 
 
     def findMethodXRef(self, units, itemId, itemAddress, refs):
@@ -55,10 +64,11 @@ class JEBTrace(IScript):
                     row_m = unit.getMethod(itemAddress)
                     impl_class = row_m.getClassType().getImplementingClass()
                     if re.search(r'(\$\d+?)+;$', impl_class.getAddress()) != None:
-                        sub_address.append((unit, impl_class.getAddress()))
+                        # sub_address.append((unit, impl_class.getAddress()))
                         clxXref = self.findClassXRef(units, impl_class.getItemId(), impl_class.getAddress())
                         sub_address.extend(clxXref)
-                        total += len(clxXref)
+                        for (unit, addresses) in clxXref:
+                            total += len(addresses)
 
         refs.getCurrentNode().setXrefCount(total)
         for (unit, addresses) in sub_address:
@@ -68,17 +78,16 @@ class JEBTrace(IScript):
                     index = indexObj.group()
                     last_index = 0-len(index)
                     method = unit.getMethod(xref_addr[0:last_index])
-                    node = refs.buildOrGetNode(method.getItemId(), method.getAddress())
-                    if node.isParsed():
+                    node = refs.buildNode(method.getItemId(), method.getAddress())
+                    if node == None:
                         refs.setCurrentNodeById(preNode.itemId)
                         return
                     else:
                         self.findMethodXRef(units,method.getItemId(), method.getAddress(),refs)
                 else:
-                    print('xref_addr -> %s' % xref_addr)
                     method = unit.getMethod(xref_addr)
-                    node = refs.buildOrGetNode(method.getItemId(), method.getAddress())
-                    if node.isParsed():
+                    node = refs.buildNode(method.getItemId(), method.getAddress())
+                    if node == None:
                         refs.setCurrentNodeById(preNode.itemId)
                         return
                     else:
@@ -92,8 +101,6 @@ class JEBTrace(IScript):
             data = ActionXrefsData()
             if unit.prepareExecution(ActionContext(unit, Actions.QUERY_XREFS, itemId, itemAddress), data):
                 if len(data.getAddresses()) > 0 :
-                    for address in data.getAddresses():
-                        print('class itemAddress xref -> %s' % address)
                     refs.append((unit, data.getAddresses()))
         return refs
 
@@ -104,6 +111,9 @@ class DataReference:
         self.head_node = head_node
         self.current_node = self.head_node
         self.current_node.is_on_parsing = True
+
+    def getNodes(self):
+        return self.nodes
     
     def addNodes(self, nodes):
         for node in nodes:
@@ -135,19 +145,27 @@ class DataReference:
         
         return False
     
-    def buildOrGetNode(self, itemId, itemAddress):
+    def addSubNode(self, node):
+        if node.itemAddress != self.current_node.itemAddress:
+            self.current_node.addNextNode(node)
+
+    def buildNode(self, itemId, itemAddress):
+
         for node in self.nodes:
             if node.itemId == itemId:
-                return node
+                self.addSubNode(node)
+                return None
         newNode = DataReferenceNode(itemId, itemAddress)
         self.nodes.append(newNode)
+        self.addSubNode(newNode)
         return newNode
 
     def getPreNodes(self, node):
-        if not node.isParsed():
-            return None
+        # if not node.isParsed():
+        #     print('node %s is not parsed yet' % node.itemAddress)
+        #     return None
         if node.itemId == self.head_node.itemId:
-            return [self.head_node.itemId]
+            return []
         else:
             pre_nodes = []
             for n in self.nodes:
@@ -163,6 +181,77 @@ class DataReference:
                 leaf.append(n)
 
         return leaf
+
+    def isHeadNode(self, node):
+        return node.itemId == self.head_node.itemId
+            
+
+    def getLineToRoot(self, line):
+        node = line.getLast()
+        print('on parse node: %s' % node.itemAddress)
+        if self.isHeadNode(node):
+            return [line]
+
+        lines = []
+        preNodes = self.getPreNodes(node)
+        for n in preNodes:
+            if not line.hasNode(n):
+                copyLine = XrefLine.copyFrom(line)
+                copyLine.addNode(n)
+                if self.isHeadNode(n):
+                    lines.append(copyLine)
+                else:
+                    subLines = self.getLineToRoot(copyLine)
+                    lines.extend(subLines)
+        return lines
+
+
+class XrefLine(object):
+    
+    def __init__(self, node=None):
+        self.line = []
+        if node != None:
+            self.line.append(node)
+
+    def hasNode(self, node):
+        for n in self.line:
+            if n.itemId == node.itemId:
+                return True
+
+        return False
+
+    def addNode(self, node):
+        if self.hasNode(node):
+            return False
+
+        self.line.append(node)
+        return True
+
+    def getLine(self):
+        return self.line
+
+    def getLast(self):
+        return self.line[-1]
+
+    def __str__(self):
+        nodeInfo = ''
+        for node in self.line:
+            nodeInfo += '    ' +node.itemAddress + '\n'
+        return '''
+        XrefLine [ 
+        %s
+        ]
+        ''' % nodeInfo
+
+    def __repr__(self):
+        return self.__str__()
+
+    @staticmethod
+    def copyFrom(xline):
+        newLine = XrefLine()
+        for node in xline.getLine():
+            newLine.addNode(node)
+        return newLine
 
 class DataReferenceNode:
 
@@ -205,10 +294,16 @@ class DataReferenceNode:
         return self.isParsed() and len(self.next_nodes) == 0
 
     def __str__(self):
-        return self.itemAddress
+        return "str:" + self.itemAddress
 
     def __repr__(self):
-        return self.itemAddress
+        sub = ''
+        for node in self.next_nodes:
+            sub += node.itemAddress + '   '
+        return '''
+        %s
+             subs: [ %s ]
+        ''' % (self.itemAddress, sub)
     
 
         
