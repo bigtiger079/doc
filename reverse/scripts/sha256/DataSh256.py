d1c737a188a0855a0b7ede2c0142a3ab997810b9
bigtiger079 <bigtiger079@gmail.com>
2019-06-10 22:40:51 +0800

    add sha256

---
+++ reverse/scripts/sha256/DataSh256.py
@@ -0,0 +1,285 @@
+import hashlib
+import struct
+from XData import Data, calcCH, calcTmp1, calcMaj, calcTmp2, createLm, DataXpr
+
+
+def out_hex(data):
+    for i in data:
+        print("%08x" % i)
+
+    print("\n")
+
+
+def rotate_left(a, k):
+    k = k % 32
+    return ((a << k) & 0xFFFFFFFF) | ((a & 0xFFFFFFFF) >> (32 - k))
+
+
+def rotate_right(a, k):
+    k = k % 32
+    return (((a >> k) & 0xFFFFFFFF) | ((a & 0xFFFFFFFF) << (32 - k))) & 0xFFFFFFFF
+
+
+def rotate_shift(a, k):
+    k = k % 32
+    return ((a >> k) & 0xFFFFFFFF)
+
+
+def P_0(X):
+    return (rotate_right(X, 7)) ^ (rotate_right(X, 18)) ^ (rotate_shift(X, 3))
+
+
+def P_1(X):
+    return (rotate_right(X, 17)) ^ (rotate_right(X, 19)) ^ (rotate_shift(X, 10))
+
+
+IV = "0x6A09E667 0xBB67AE85 0x3C6EF372 0xA54FF53A 0x510E527F 0x9B05688C 0x1F83D9AB 0x5BE0CD19"
+
+IV = IV.replace("0x", "")
+
+IV = int(IV.replace(" ", ""), 16)
+
+K = """0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 """
+
+K = K.replace("\n", "")
+K = K.replace(", ", "")
+K = K.replace(" ", "")
+K = K.replace("0x", "")
+K = int(K, 16)
+
+a = []
+for i in range(0, 8):
+    a.append(0)
+    a[i] = (IV >> ((7 - i) * 32)) & 0xFFFFFFFF
+
+IV = a
+
+k = []
+
+for i in range(0, 64):
+    k.append(0)
+    k[i] = (K >> ((63 - i) * 32)) & 0xFFFFFFFF
+
+K = k
+
+indexValue = {}
+
+
+def CF(V_i, B_i):
+    W = []
+    # 构造64个字
+    # 其中前16个字直接由消息的第i个块分解得到
+
+    for j in range(0, 16):
+        W.append(0)
+        unpack_list = struct.unpack(">I", B_i[j * 4:(j + 1) * 4])
+        W[j] = unpack_list[0]
+        if j == 0:
+            indexValue['0'] = Data(xpr=DataXpr(ltype=0, lm=(lambda x: x)))
+        else:
+            indexValue[str(j)] = Data(num=W[j])
+
+    for j in range(16, 64):
+        W.append(0)
+        s0 = P_0(W[j - 15])
+        s1 = P_1(W[j - 2])
+        W[j] = (W[j - 16] + s0 + W[j - 7] + s1) & 0xFFFFFFFF
+
+        xdata = indexValue[str(j - 16)] + indexValue[str(j - 15)].getP0() + indexValue[str(j - 7)] + indexValue[str(j - 2)].getP1()
+        indexValue[str(j)] = xdata.dAnd(0xFFFFFFFF)
+
+        str1 = "%08x" % W[j]
+
+    W_1 = []
+    A, B, C, D, E, F, G, H = V_i
+    """
+    print "00",
+    out_hex([A, B, C, D, E, F, G, H])
+    """
+    """
+    S1 := (e rightrotate 6) xor (e rightrotate 11) xor (e rightrotate 25)
+    ch := (e and f) xor ((not e) and g)
+    temp1 := h + S1 + ch + k[i] + w[i]
+    S0 := (a rightrotate 2) xor (a rightrotate 13) xor (a rightrotate 22)
+    maj := (a and b) xor (a and c) xor (b and c)
+    temp2 := S0 + maj
+
+    h := g
+    g := f
+    f := e
+    e := d + temp1
+    d := c
+    c := b
+    b := a
+    a := temp1 + temp2
+    T1 = H + LSigma_1(E) + Conditional(E, F, G) + K[i] + W[i];
+    T2 = LSigma_0(A) + Majority(A, B, C);
+    """
+
+    encA = Data(num=A)
+    encB = Data(num=B)
+    encC = Data(num=C)
+    encD = Data(num=D)
+    encE = Data(num=E)
+    encF = Data(num=F)
+    encG = Data(num=G)
+    encH = Data(num=H)
+
+    # 进行64次循环加密
+    for j in range(0, 64):
+        SS1 = rotate_right(E, 6) ^ rotate_right(E, 11) ^ rotate_right(E, 25)
+        SS0 = rotate_right(A, 2) ^ rotate_right(A, 13) ^ rotate_right(A, 22)
+        ch = (E & F) ^ ((~E) & G)
+        temp1 = (H + SS1 + ch + K[j] + W[j]) & 0xFFFFFFFF
+        maj = (A & B) ^ (A & C) ^ (B & C)
+        temp2 = (SS0 + maj) & 0xFFFFFFFF
+
+        H = G
+        G = F
+        F = E
+        E = (D + temp1)
+        D = C
+        C = B
+        B = A
+        A = (temp1 + temp2)
+
+        A = A & 0xFFFFFFFF
+        B = B & 0xFFFFFFFF
+        C = C & 0xFFFFFFFF
+        D = D & 0xFFFFFFFF
+        E = E & 0xFFFFFFFF
+        F = F & 0xFFFFFFFF
+        G = G & 0xFFFFFFFF
+        H = H & 0xFFFFFFFF
+
+        xss1 = encE.getSS1()
+        xss0 = encA.getSS0()
+        xch = calcCH(encE, encF, encG)
+        xtmp1 = calcTmp1(encH, xss1, xch, K[j], indexValue[str(j)])
+        xmaj = calcMaj(encA, encB, encC)
+        xtmp2 = calcTmp2(xss0, xmaj)
+
+        encH = encG
+        encG = encF
+        encF = encE
+        encE = (encD + xtmp1)
+        encD = encC
+        encC = encB
+        encB = encA
+        encA = (xtmp1 + xtmp2)
+
+        encA = encA.dAnd(0xFFFFFFFF)
+        encB = encB.dAnd(0xFFFFFFFF)
+        encC = encC.dAnd(0xFFFFFFFF)
+        encD = encD.dAnd(0xFFFFFFFF)
+        encE = encE.dAnd(0xFFFFFFFF)
+        encF = encF.dAnd(0xFFFFFFFF)
+        encG = encG.dAnd(0xFFFFFFFF)
+        encH = encH.dAnd(0xFFFFFFFF)
+        """
+        str1 = "%02d" % j
+        if str1[0] == "0":
+                str1 = ' ' + str1[1:]
+        print str1,
+        out_hex([A, B, C, D, E, F, G, H])
+        """
+
+    print("After Encode: ")
+    print(A, B, C, D, E, F, G, H)
+
+    # for j in range(0, 64):
+    #     Ar = B
+    #     Br = C
+    #     Cr = D
+    #     Er = F
+    #     Fr = G
+    #     Gr = H
+    #     SS0 = rotate_right(Ar,2) ^ rotate_right(Ar,13) ^ rotate_right(Ar,22)
+    #     SS1 = rotate_right(Er,6) ^ rotate_right(Er,11) ^ rotate_right(Er,25)
+    #     ch  = (Er & Fr) ^ ((~Er) & Gr)
+    #     maj = (Ar & Br) ^ (Ar & Cr) ^ ( Br & Cr)
+    #     t2 = (SS0 + maj) & 0xFFFFFFFF
+    #     t1 = A - t2
+    #     Dr = E - t1
+    #     #t1 = (Hr + SS1 + ch + K[63 - j] + W[63 - j]) & 0xFFFFFFFF
+    #     Hr + W[63 - j] = t1 - SS1 - K[63 - j] - ch
+
+    V_i_1 = []
+    V_i_1.append((A + V_i[0]) & 0xFFFFFFFF)
+    V_i_1.append((B + V_i[1]) & 0xFFFFFFFF)
+    V_i_1.append((C + V_i[2]) & 0xFFFFFFFF)
+    V_i_1.append((D + V_i[3]) & 0xFFFFFFFF)
+    V_i_1.append((E + V_i[4]) & 0xFFFFFFFF)
+    V_i_1.append((F + V_i[5]) & 0xFFFFFFFF)
+    V_i_1.append((G + V_i[6]) & 0xFFFFFFFF)
+    V_i_1.append((H + V_i[7]) & 0xFFFFFFFF)
+
+    XV_i_1 = []
+    XV_i_1.append((encA + V_i[0]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encB + V_i[1]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encC + V_i[2]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encD + V_i[3]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encE + V_i[4]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encF + V_i[5]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encG + V_i[6]).dAnd(0xFFFFFFFF))
+    XV_i_1.append((encH + V_i[7]).dAnd(0xFFFFFFFF))
+    print(encA.xpr)
+
+    return V_i_1
+
+
+def hash_msg(msg):
+    # 信息预处理
+    msgLen = len(msg)
+    reserve1 = msgLen % 64
+    # 附加填充比特 1 0 0 0  ....   488
+    preProcessingMsg = msg.encode() + struct.pack("B", 128)  # unsignedchar 1000 0000
+    reserve1 = reserve1 + 1
+    for i in range(reserve1, 56):
+        preProcessingMsg = preProcessingMsg + struct.pack("B", 0)
+
+    bit_length = (msgLen) * 8
+    bit_length_string = struct.pack(">Q", bit_length)  # big-endian unsignedlonglong
+    # 附加长度值
+    preProcessingMsg = preProcessingMsg + bit_length_string
+
+    print(f'preProcessingMsg length: {len(preProcessingMsg)}')
+    group_count = int(len(preProcessingMsg) / 64)
+    print(f'group count: {group_count}')
+
+    # break message into 512-bit chunks
+    B = []
+    for i in range(0, group_count):
+        B.append(0)
+        B[i] = preProcessingMsg[i * 64:(i + 1) * 64]
+
+    print(f'chunks: {B}')
+
+    V = []
+    V.append(0)
+    V[0] = IV
+    # mapping
+    for i in range(0, group_count):
+        V.append(0)
+        V[i + 1] = CF(V[i], B[i])
+
+    # print(i)
+    return V[group_count]
+
+
+src = "yyyyWKPjdffgbP8nuTk0"
+print(src)
+y = hash_msg(src)
+print("result: ")
+print(y)
+out_hex(y)
+
+sh = hashlib.sha256()
+print(hashlib.sha256(src.encode()).hexdigest())
---
+++ reverse/scripts/sha256/XData.py
@@ -0,0 +1,304 @@
+class DataXpr():
+
+    def __init__(self, ltype, lm, param=[]):
+        self.lm = lm
+        self.ltype = ltype
+        self.result = -1
+
+    def setReslut(self, r):
+        self.result = r
+
+    def getX(self):
+        if self.ltype == 0:
+            return self.result
+        elif self.ltype == 6:
+            pass
+
+    def getLm(self):
+        return self.lm
+
+    def getType(self):
+        return self.ltype
+
+    def __add__(self, xpr):
+        return createLm(3, self.getLm(), xpr.getLm())
+
+
+def createLm(ltype, *args):
+
+    if ltype == 0:
+        return DataXpr(ltype=ltype, lm=(lambda x: x))
+    elif ltype == 1:
+        return DataXpr(ltype=ltype, lm=(lambda x: (rotate_right(args[0] + x, 7)) ^ (rotate_right(args[0] + x, 18)) ^ (rotate_shift(args[0] + x, 3))))
+    elif ltype == 2:
+        return DataXpr(ltype=ltype, lm=(lambda x: ((rotate_right(args[0] + x, 17)) ^ (rotate_right(args[0] + x, 19)) ^ (rotate_shift(args[0] + x, 10)))))
+    elif ltype == 3:
+        return DataXpr(ltype=ltype, lm=(lambda x: args[0](x) + args[1](x)))
+    elif ltype == 4:
+        return DataXpr(ltype=ltype, lm=(lambda x: (((args[0](x) + args[1]) << args[2]) & 0xFFFFFFFF)))
+    elif ltype == 5:
+        return DataXpr(ltype=ltype, lm=(lambda x: (((args[0](x) + args[1]) >> args[2]) & 0xFFFFFFFF)))
+    elif ltype == 6:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0](x) + args[1]) ^ args[2]))
+    elif ltype == 7:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0](x) + args[1]) ^ (args[2](x) + args[3])))
+    elif ltype == 8:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0](x) + args[1]) & args[2]))
+    elif ltype == 9:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0](x) + args[1]) & args[2]))
+    elif ltype == 10:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0](x) + args[1]) & (args[2](x) + args[3])))
+    elif ltype == 11:
+        return DataXpr(ltype=ltype, lm=(lambda x: ~(args[0](x) + args[1])))
+    elif ltype == 12:
+        return DataXpr(ltype=ltype, lm=(lambda x: (((args[0].rsh(x) & 0xFFFFFFFF) | (args[0].lsh(32 - x) & 0xFFFFFFFF)) & 0xFFFFFFFF)))
+    elif ltype == 13:
+        return DataXpr(ltype=ltype, lm=(lambda x: ((args[0].lsh(x) & 0xFFFFFFFF) | (args[0].rsh(32 - x) & 0xFFFFFFFF))))
+    elif ltype == 14:
+        return DataXpr(ltype=ltype, lm=(lambda x: (args[0].rsh(x) & 0xFFFFFFFF)))
+
+
+class Data():
+
+    def __init__(self, xpr=lambda x: 0, num=0):
+        self.xpr = xpr
+        self.num = num
+
+    def getXpr(self):
+        return self.xpr
+
+    def getNum(self):
+        return self.num
+
+    def isXprType(self):
+        return self.xpr is not None
+
+    def isNumType(self):
+        return self.xpr is None
+
+    def data(self):
+        if self.isXprType:
+            return self.xpr + self.num
+        else:
+            return self.num
+
+    def setDefiner(self, definer):
+        self.definer = definer
+
+    def getDefiner(self):
+        return self.definer
+
+    def __add__(self, value):
+        if type(value) == int:
+            if self.isNumType():
+                return Data(num=((self.num + value) & 0xFFFFFFFF))
+            else:
+                return Data(xpr=self.xpr, num=((self.num + value) & 0xFFFFFFFF))
+        else:
+            if self.isNumType() and value.isNumType():
+                return Data(num=((self.num + value.num) & 0xFFFFFFFF))
+            else:
+                num = self.num + value.num
+                # result = Data(xpr=lambda x: self.xpr(x) + value.xpr(x), num=num)
+                # return result
+                return Data(xpr=createLm(3, self.xpr, value.xpr), num=num)
+
+    # def __or__(self, rhs):
+    #     if self.isNumType():
+    #         return Data(num=self.num | rhs)
+    #     else:
+    #         return Data(xpr)
+
+    def __xor__(self, rhs):
+        if type(rhs) == int:
+            if self.isNumType():
+                return Data(num=self.num ^ rhs)
+            else:
+                return Data(xpr=lambda x: (self.xpr(x) + self.num) ^ rhs)
+        else:
+            if self.isNumType():
+                return Data(xpr=lambda x: (self.num) ^ (rhs.xpr(x) + rhs.num))
+            else:
+                return Data(xpr=createLm(7, self.xpr, self.num, rhs.xpr, rhs.num))
+
+    def lsh(self, x):
+        if self.isNumType():
+            r = (self.num << (x % 32)) & 0xFFFFFFFF
+            return Data(num=r)
+        else:
+            # return Data(xpr=lambda y: (((self.xpr(y) + self.num) << x) & 0xFFFFFFFF))
+            return Data(xpr=createLm(4, self.xpr, self.num))
+
+    def rsh(self, x):
+        if self.isNumType():
+            r = (self.num >> (x % 32)) & 0xFFFFFFFF
+            return Data(num=r)
+        else:
+            # return Data(xpr=lambda y: (((self.xpr(y) + self.num) >> x) & 0xFFFFFFFF))
+            return Data(xpr=createLm(5, self.xpr, self.num))
+
+    def xor(self, k):
+        if type(k) == int:
+            if self.isNumType():
+                return Data(num=self.num ^ k)
+            else:
+                # return Data(xpr=lambda x: (self.xpr(x) + self.num) ^ k)
+                return Data(xpr=createLm(6, self.xpr, self.num, k))
+        else:
+            if self.isNumType():
+                # return Data(xpr=lambda x: (k.xpr(x) + k.num) ^ self.num)
+                return Data(xpr=createLm(6, k.xpr, k.num, self.num))
+            else:
+                # return Data(xpr=lambda x: (self.xpr(x) + self.num) ^ (k.xpr(x) + k.num))
+                return Data(xpr=createLm(7, self.xpr, self.num, k.xpr, k.num))
+
+    def xAnd(self, k):
+        if type(k) == int:
+            if self.isNumType():
+                return Data(num=self.num & k)
+            else:
+                # return Data(xpr=lambda x: (self.xpr(x) + self.num) & k)
+                return Data(xpr=createLm(8, self.xpr, self.num, k))
+        else:
+            if self.isNumType():
+                # return Data(xpr=lambda x: (k.xpr(x) + k.num) & self.num)
+                return Data(xpr=createLm(9, k.xpr, k.num, self.num))
+            else:
+                # return Data(xpr=lambda x: (self.xpr(x) + self.num) & (k.xpr(x) + k.num))
+                return Data(xpr=createLm(10, self.xpr, self.num, k.xpr, k.num))
+
+    def __invert__(self):  # ~x
+        if self.isNumType():
+            return Data(num=~self.num)
+        else:
+            return Data(xpr=createLm(11, self.xpr, self.num))
+
+    def xNot(self):
+        if self.isNumType():
+            return Data(num=~self.num)
+        else:
+            # return Data(xpr=lambda x: ~(self.xpr(x) + self.num))
+            return Data(xpr=createLm(11, self.xpr, self.num))
+
+    def rotateRight(self, x):
+        x = x % 32
+        if self.isNumType():
+            return Data(num=rotate_right(self.num))
+        else:
+            # return Data(xpr=lambda y: (((self.rsh(y) & 0xFFFFFFFF ) | (self.lsh(32-y) & 0xFFFFFFFF )) & 0xFFFFFFFF ))
+            return Data(xpr=createLm(12, self))
+
+    def rotateLeft(self, x):
+        x = x % 32
+        if self.isNumType():
+            return Data(num=rotate_left(self.num))
+        else:
+            # return Data( xpr=lambda y: ((self.lsh(y) & 0xFFFFFFFF ) | (self.rsh(32-y) & 0xFFFFFFFF )) )
+            return Data(xpr=createLm(13, self))
+
+    def rotateShift(self, x):
+        x = x % 32
+        if self.isNumType():
+            return Data(num=((self.num >> x) & 0xFFFFFFFF))
+        else:
+            # return Data(xpr=lambda y: (self.rsh(y) & 0xFFFFFFFF))
+            return Data(xpr=createLm(14, self))
+
+
+def funcCH(e, f, g):
+    # (E & F) ^ ((~E) & G)
+    if e.isNumType() and f.isNumType() and g.isNumType():
+        r = (e.num & f.num) ^ ((~e.num) & g.num)
+        return Data(num=r)
+    else:
+        de = Data(num=e) if type(e) == int else e
+        df = Data(num=f) if type(f) == int else f
+        dg = Data(num=g) if type(g) == int else g
+        return de.xAnd(df).xor(de.xNot().xAnd(dg))
+
+
+def calcTmp1(h, s1, ch, k, w):
+    # (H + SS1 + ch + K[j] + W[j]) & 0xFFFFFFFF
+    if h.isNumType() and s1.isNumType() and ch.isNumType() and w.isNumType():
+        r = (h.num + s1.num + ch.num + k + w.num) & 0xFFFFFFFF
+        return Data(num=r)
+    else:
+        dh = Data(num=h) if type(h) == int else h
+        ds1 = Data(num=s1) if type(s1) == int else s1
+        dch = Data(num=ch) if type(ch) == int else ch
+        dw = Data(num=w) if type(w) == int else w
+        dk = Data(num=k)
+        return (dh + ds1 + dch + dk + dw).xAnd(0xFFFFFFFF)
+
+
+def funcMaj(a, b, c):
+    if a.isNumType() and b.isNumType() and c.isNumType():
+        r = (a.num & b.num) ^ (a.num & c.num) ^ (b.num & c.num)
+        return Data(num=r)
+    else:
+        da = Data(num=a) if type(a) == int else a
+        db = Data(num=b) if type(b) == int else b
+        dc = Data(num=c) if type(c) == int else c
+        return (da.xAnd(db)).xor(da.xAnd(dc)).xor(db.xAnd(dc)).xAnd(0xFFFFFFFF)
+
+
+def funcBSIG0(x):
+    if x.isNumType():
+        return Data(num=rotate_right(x, 2) ^ (rotate_right(x, 13)) ^ (rotate_right(x, 22)))
+    else:
+        return x.rotateRight(2).xor(x.rotateRight(13)).xor(x.rotateRight(22))
+
+
+def funcBSIG1(x):
+    if x.isNumType():
+        return Data(num=rotate_right(x, 6) ^ (rotate_right(x, 11)) ^ (rotate_right(x, 25)))
+    else:
+        return x.rotateRight(6).xor(x.rotateRight(11)).xor(x.rotateRight(25))
+
+
+def funcSSIG0(x):
+    if x.isNumType():
+        return Data(num=rotate_right(x, 7) ^ (rotate_right(x, 18)) ^ (rotate_shift(x, 3)))
+    else:
+        return x.rotateRight(7).xor(x.rotateRight(8)).xor(x.rotateShift(13))
+
+
+def funcSSIG1(x):
+    if x.isNumType():
+        return Data(num=rotate_right(x, 17) ^ (rotate_right(x, 19)) ^ (rotate_shift(x, 10)))
+    else:
+        return x.rotateRight(17).xor(x.rotateRight(19)).xor(x.rotateShift(10))
+
+
+def calcTmp2(ss0, maj):
+    # (SS0 + maj) & 0xFFFFFFFF
+    if ss0.isNumType() and maj.isNumType():
+        r = (ss0.num + maj.num) & 0xFFFFFFFF
+        return Data(num=r)
+    else:
+        dss0 = Data(num=ss0) if type(ss0) == int else ss0
+        dmaj = Data(num=maj) if type(maj) == int else maj
+        return (dss0 + dmaj).xAnd(0xFFFFFFFF)
+
+
+def rotate_left(a, k):
+    k = k % 32
+    return ((a << k) & 0xFFFFFFFF) | ((a & 0xFFFFFFFF) >> (32 - k))
+
+
+def rotate_right(a, k):
+    k = k % 32
+    return (((a >> k) & 0xFFFFFFFF) | ((a & 0xFFFFFFFF) << (32 - k))) & 0xFFFFFFFF
+
+
+def rotate_shift(a, k):
+    k = k % 32
+    return ((a >> k) & 0xFFFFFFFF)
+
+
+def P_0(X):
+    return (rotate_right(X, 7)) ^ (rotate_right(X, 18)) ^ (rotate_shift(X, 3))
+
+
+def P_1(X):
+    return (rotate_right(X, 17)) ^ (rotate_right(X, 19)) ^ (rotate_shift(X, 10))
